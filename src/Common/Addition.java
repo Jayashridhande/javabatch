437de50d662b0c11f2a70a3013a13f473fee76ec
jayashridhande <jayashridhande13@gmail.com>
2022-09-14 18:00:08 +0530

    arithmatic opp

---
+++ src/Common/Addition.java
@@ -0,0 +1,18 @@
+package Common;
+
+public class Addition {
+
+    public static void main(String[] args){
+        
+        int a =10;
+        int b= 5;
+        int c= a+b;
+        int d = a*b;
+        int e = a-b;
+        int f= a/b;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
---
+++ src/Common/Test.java
@@ -0,0 +1,11 @@
+package Common;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Good Morning.");
+        System.out.println("It is awesome!");
+        System.out.println(3+8);
+    }
+}
